1a56fcbcf397e15bfeb4fa92ebdb6880f8e946ba
Matthew Keplinger <matthew.keplinger85@gmail.com>
2021-09-10 13:19:08 -0400

    MusicViewer completed and shows on page; needs formatting with rest of page content upon completion

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,1 +1,36 @@
 import React, { Component } from 'react';
+import './App.css';
+import axios from 'axios';
+import MusicViewer from './MusicViewer/MusicViewer';
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            songs : []
+          }
+    }
+
+    componentDidMount(){
+        this.getSongs();
+    }
+
+    async getSongs(){
+        let response = await axios.get('http://127.0.0.1:8000/music/');
+        console.log(response);
+        this.setState({
+            songs: response.data
+        });
+    }
+
+    render() { 
+        return (
+            <div class = 'center'>
+                <h1>Music Library</h1>
+                <MusicViewer songs = {this.state.songs} />
+            </div>
+         );
+    }
+}
+ 
+export default App ;
---
+++ src/components/MusicViewer/MusicViewer.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import './MusicViewer.css';
+
+const MusicViewer  = (props) => {
+    return (
+        <table>
+            <thead>
+                <th>Title</th>
+                <th>Artist</th>
+                <th>Album</th>
+                <th>Genre</th>
+                <th>Release Date</th>
+            </thead>
+            {props.songs.map((song) => {
+                return(
+                    <tr>
+                        <td>{song.title}</td>
+                        <td>{song.artist}</td>
+                        <td>{song.album}</td>
+                        <td>{song.genre}</td>
+                        <td>{song.release_date}</td>
+                    </tr>
+                );
+            })}
+        </table>
+    );
+}
+ 
+export default MusicViewer;
